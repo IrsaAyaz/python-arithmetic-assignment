301a8ae157c6ddf6db98248cc3315e5d02ad36a0
Irsa Ayaz <irsaayaz.525@gmail.com>
2025-05-14 14:40:11 +0500

    Add files via upload

---
+++ arithmetic_assignment.py
@@ -0,0 +1,41 @@
+#Task 1: Addition
+x = 7
+y = 5
+result = x + y
+print(f"{x} + {y} = {result}")
+
+#Task 2: Subtraction
+x = 7
+y = 5
+result = x - y
+print(f"{x} - {y} = {result}")
+
+#Task 3: Multiplication
+x = 7
+y = 5
+result = x * y
+print(f"{x} * {y} = {result}")
+
+#Task 4: Division
+x = 7
+y = 5
+result = x / y
+print(f"{x} / {y} = {result}")
+
+#Task 5: Floor Division
+x = 7
+y = 5
+result = x // y
+print(f"{x} // {y} = {result}")
+
+#Task 6: Moddulus
+x = 7
+y = 5
+result = x % y
+print(f"{x} % {y} = {result}")
+
+#Task 7: Exponentiation
+x = 7
+y = 5
+result = x ** y
+print(f"{x} ** {y} = {result}")
